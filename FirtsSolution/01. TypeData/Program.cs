6e7f33122b9d56cfb486524c81bebb25435e8cf7
Halil <dospatliyski@gmail.com>
2025-11-04 10:58:41 +0200

    dewd

---
+++ FirtsSolution/01. TypeData/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._TypeData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //тип   име   стойност
+            string name = "Kiril Yordanov";
+            string grad = "Sofia";
+            int age = 17;
+            double uspeh = 5.45;
+
+
+            //Интерполация
+            Console.WriteLine($"My name is {name}. I am {age} years old. I am from {grad}.");
+
+
+
+        }
+    }
+}
